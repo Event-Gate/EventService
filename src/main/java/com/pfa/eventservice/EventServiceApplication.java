42a5a22b439c393b28065ca176da8a258a66eacd
apostorial <t.elouadrassi@gmail.com>
2025-01-16 06:00:13 +0100

    Removed unnecessary code and added an annotation to enable Feign Client.

--- src/main/java/com/pfa/eventservice/EventServiceApplication.java
+++ src/main/java/com/pfa/eventservice/EventServiceApplication.java
@@ -1,30 +1,12 @@
 package com.pfa.eventservice;
 
-import com.pfa.eventservice.dao.entities.Creator;
-import com.pfa.eventservice.dao.repositories.CreatorRepository;
-import com.pfa.eventservice.dao.repositories.EventRepository;
-import com.pfa.eventservice.dto.CreatorDto;
-import com.pfa.eventservice.service.CreatorService;
-import com.pfa.eventservice.service.EventService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication @EnableFeignClients
 public class EventServiceApplication {
-    @Autowired
-    private CreatorRepository creatorRepository;
-
-    @Autowired
-    private EventRepository eventRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
     }
-
-
-
-    }
-
+}
